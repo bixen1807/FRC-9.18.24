6d728f7a3590ed53bca9c71d27a40330c0045a32
8590_Fan <123890489+ConnorGeraghty1@users.noreply.github.com>
2024-09-18 12:55:26 -0400

    Create IntakePracticeCommand.java

---
+++ src/main/java/frc/robot/Commands/IntakePracticeCommand.java
@@ -0,0 +1,49 @@
+package frc.robot.Commands;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.Subsystems.Arm;
+import frc.robot.Subsystems.Intake;
+import frc.robot.Subsystems.Lights;
+
+public class IntakeCommand extends Command {
+    private Arm arm;
+    private Intake intake;
+    private boolean isFinished = false;
+    public int elapsed = 0;
+    public boolean triggered = false;
+    public Lights lights;
+    public IntakeCommand(){
+        addRequirements(Constants.intake);
+
+    }
+    @Override
+    public void initialize(){
+        intake = Constants.intake;
+        triggered = false;
+        elapsed = 0;
+        arm = Constants.arm;
+        arm.setDesired(5.5);
+        addRequirements(Constants.arm);
+        lights = Constants.lights;
+    }   
+
+    @Override
+    public void execute(){
+        //FILL IN THIS COMMAND WITH CODE TO Make THE PROGRAM RUN
+    }
+    @Override
+    public boolean isFinished(){
+         
+        return isFinished;
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        if(interrupted) {
+            lights.off();
+        }
+        Constants.intake.stop();
+        isFinished = false;
+    }
+}
